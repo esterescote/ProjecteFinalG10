9bcf16566a77f61483ec6c961cc86f004306995d
ester <ester.martinez.escote@estudiantat.upc.edu>
2025-04-04 11:31:58 +0200

    supabase.js

--- App.js
+++ App.js
@@ -8,11 +8,11 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-  <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home"> 
-    <Stack.Screen name="Home" component={HomeScreen} />
-    <Stack.Screen name="Details" component={DetailsScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home"> 
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
