607f5a09447d53e753e2ffe6bd5d616463c95cc6
marinamagi <marina.magi@estudiantat.upc.edu>
2025-05-31 11:39:27 +0200

    nou canvi

--- supabase.js
+++ supabase.js
@@ -3,8 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_URL = 'https://wlaumweodxeqrwktfqlg.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsYXVtd2VvZHhlcXJ3a3RmcWxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0MTMwMjksImV4cCI6MjA1ODk4OTAyOX0.4lzGcn8jbgUKHN6Wi0m9wHS3edK5MaL-bOVK-7_Gcuo';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  realtime: {
-    enabled: false
-  }
-});
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
